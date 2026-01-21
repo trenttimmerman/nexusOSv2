93488e8b7d73fe894be0f51338a00569c02e3e15
trenttimmerman <trent.timmerman@gmail.com>
2026-01-21 01:24:33 +0000

    Add cursor-default to all marketing pages to prevent I-beam cursor on text

--- components/pages/API.tsx
+++ components/pages/API.tsx
@@ -30,7 +30,7 @@ export const API: React.FC = () => {
     ];
 
     return (
-        <div className="min-h-screen bg-black text-white selection:bg-cyan-500/30">
+        <div className="min-h-screen bg-black text-white selection:bg-cyan-500/30 cursor-default">
             {/* Background Aurora Effect */}
             <div className="absolute inset-0 -z-10 overflow-hidden">
                 <div className="absolute top-0 left-1/4 w-96 h-96 bg-purple-600 rounded-full filter blur-[150px] opacity-50 animate-breathe"></div>
--- components/pages/Features.tsx
+++ components/pages/Features.tsx
@@ -50,7 +50,7 @@ export const Features: React.FC = () => {
     ];
 
     return (
-        <div className="min-h-screen bg-black text-white selection:bg-cyan-500/30">
+        <div className="min-h-screen bg-black text-white selection:bg-cyan-500/30 cursor-default">
             {/* Background Aurora Effect */}
             <div className="absolute inset-0 -z-10 overflow-hidden">
                 <div className="absolute top-0 left-1/4 w-96 h-96 bg-purple-600 rounded-full filter blur-[150px] opacity-50 animate-breathe"></div>
